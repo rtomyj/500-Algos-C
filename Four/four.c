709a92abf3dce37a0e691cfe2cb87a37be6bd45b
Javi Gomez <rtomyj@gmail.com>
2019-02-22 09:38:56 -0600

    deleted binary file, modified function to work with arrays of varying length

--- Four/four.c
+++ Four/four.c
@@ -5,34 +5,34 @@
 	Since merging in C is a pain, initializing the sorted array with the right most value first
 	then adding the leftmost elements as needed is a better approach.
 */
-int* method_one(int items[], int leftMostValue)
+void method_one(int items[], int sizeArr, int sortedItems[], int leftMostValue)
 {
 	int rightMostValue = (leftMostValue == 0) ? 1 : 0;
-	const int sizeArr = sizeof(items);
-	int total = 0;
-	static int sortedItems[ sizeArr ];	// static keyword allows arrays to be returned by method
+	int leftMostIndex = 0, rightMostIndex = sizeArr -1;
 
-	for (int i = 0; i < sizeArr; i++)
-	{
-		sortedItems[i] = rightMostValue;
-	}
 	
 	for (int i = 0; i < sizeArr; i++)
 	{
 		if (items[i] == leftMostValue)
 		{
-			sortedItems[total] = leftMostValue;
-			++total;
+			sortedItems[leftMostIndex] = leftMostValue;
+			++leftMostIndex;
 		}
+		else
+		{
+			sortedItems[rightMostIndex] = rightMostValue;
+			--rightMostIndex;
+			
+		}
+		
 	}
-	return sortedItems;
 
 }
 
 
-void printArr(int arr[])
+void printArr(int arr[], int arrSize)
 {
-	for (int i = 0; i < sizeof(arr); i++)
+	for (int i = 0; i < arrSize; i++)
 	{
 		printf("%lu \n", arr[i]);
 	}
@@ -40,10 +40,12 @@ void printArr(int arr[])
 
 int main()
 {
-	int items[8] = { 1, 0, 1, 0, 1, 0, 0, 1 };
+	int items[] = { 1, 0, 1, 0, 1, 0, 0, 1 };
+	const int arrSize = sizeof(items) / sizeof(int);
+	int sortedItems[arrSize];
 
-	int* sortedItems = method_one(items, 1);
-	printArr(sortedItems);
+	method_one(items, arrSize, sortedItems, 0);
+	printArr(sortedItems, arrSize);
 
 	return 0;
 }
