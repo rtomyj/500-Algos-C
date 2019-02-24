b92fc1c6ef02cfddd7a0ba9591fca632b83dcbb7
Javi Gomez <rtomyj@gmail.com>
2019-02-23 20:06:04 -0600

    new algo, documentation, smaller datatypes

--- Four/four.c
+++ Four/four.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-// int sortedItems[ sizeof(items) / sizeof(int) ] = { 0 };
 
 /*
 	Merging in C isn't simple. This approach has two arrays being passed as arguments.
@@ -7,12 +6,12 @@
 	Unsorted array is traversed and depending on how the user wants the binary array to be sorted
 	at position i will be pushed to the left or right side of the sorted array.
 */
-void method_one(int items[], int sizeArr, int sortedItems[], int leftMostValue)
+void method_one(unsigned char items[], unsigned char sizeArr, unsigned char sortedItems[], unsigned char leftMostValue)
 {
-	int rightMostValue = (leftMostValue == 0) ? 1 : 0;	// determinse the right most value (!leftMostValue)
-	int leftMostIndex = 0, rightMostIndex = sizeArr -1;	// starting positions for inserting left and right values in sorte darray
+	unsigned const char rightMostValue = (leftMostValue == 0) ? 1 : 0;	// determinse the right most value (!leftMostValue)
+	unsigned char leftMostIndex = 0, rightMostIndex = sizeArr -1;	// starting positions for inserting left and right values in sorte darray
 	
-	for (int i = 0; i < sizeArr; i++)
+	for (unsigned char i = 0; i < sizeArr; i++)
 	{
 		if (items[i] == leftMostValue)
 		{
@@ -23,7 +22,6 @@ void method_one(int items[], int sizeArr, int sortedItems[], int leftMostValue)
 		{
 			sortedItems[rightMostIndex] = rightMostValue;
 			--rightMostIndex;
-			
 		}
 		
 	}
@@ -32,24 +30,52 @@ void method_one(int items[], int sizeArr, int sortedItems[], int leftMostValue)
 
 
 /*
+	Fills the left most values on top fo the original array.
+	Counts how many left most values there are and stores it in totalLeftMost.
+	Fills the right most values starting at totalLeftMost position.
+*/
+void method_two(unsigned char items[], unsigned size, unsigned char leftMostValue)
+{
+	unsigned char totalLeftMost = 0;
+	unsigned const char rightMostValue = (leftMostValue == 0) ? 1 : 0;
+
+	for (unsigned char i = 0; i < size; i++)
+	{
+		if (items[i] == leftMostValue)
+		{
+			items[totalLeftMost] = leftMostValue;
+			++totalLeftMost;
+		}
+	}
+
+	for (unsigned int i = totalLeftMost; i < size; i++)
+	{
+		items[i] = rightMostValue;
+	}
+}
+
+
+/*
 	Prints contents of array given size and contents.
 */
-void printArr(int arr[], int arrSize)
+void printArr(unsigned char arr[], unsigned char arrSize)
 {
-	for (int i = 0; i < arrSize; i++)
+	for (unsigned char i = 0; i < arrSize; i++)
 	{
 		printf("%i\n", arr[i]);
 	}
 }
 
+
 int main()
 {
-	int items[] = { 1, 0, 1, 0, 1, 0, 0, 1 };
-	const int arrSize = sizeof(items) / sizeof(int);
-	int sortedItems[arrSize];
+	unsigned char items[] = { 1, 0, 1, 0, 1, 0, 0, 1 };
+	const unsigned char arrSize = sizeof(items) / sizeof(char);
+	unsigned char sortedItems[arrSize];
 
-	method_one(items, arrSize, sortedItems, 0);
-	printArr(sortedItems, arrSize);
+	//method_one(items, arrSize, sortedItems, 0);
+	method_two(items, arrSize, 1);
+	printArr(items, arrSize);
 
 	return 0;
 }
