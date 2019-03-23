4bb83a1f1aa4b77d01ab4be7c794bccea8d0a839
Javi Gomez <rtomyj@gmail.com>
2019-03-23 12:28:00 -0500

    now prints whole array in bracket form

--- libs/print.c
+++ libs/print.c
@@ -6,33 +6,52 @@
 */
 void printArr(void* startingMemLocation, unsigned int arrSize, INT_TYPE intType)
 {
+	if (arrSize < 1)
+		return;
+	
+	printf("[");
 
-	for (unsigned int i = 0; i < arrSize; i++)
+	switch (intType)
+		{
+			case INT:
+				printf("%d", *(int*)startingMemLocation);
+				break;
+			case SHORT:
+				printf("%d", *(short*)startingMemLocation);
+				break;
+			case CHAR:
+				printf("%d", *(char*)startingMemLocation);
+				break;
+		
+			default:
+				break;
+		}
+
+	for (unsigned int i = 1; i < arrSize; i++)
 	{
 		int* nextIntLocation;
 		short* nextShortLocation;
 		char* nextCharLocation;
 
+
 		switch (intType)
 		{
 			case INT:
-				nextIntLocation = (int*) startingMemLocation
-			 + i;
-				printf("%d \n", *nextIntLocation);
+				nextIntLocation = (int*) startingMemLocation + i;
+				printf(", %d", *nextIntLocation);
 				break;
 			case SHORT:
-				nextShortLocation = (short*) startingMemLocation
-			 + i;
-				printf("%d \n", *nextShortLocation);
+				nextShortLocation = (short*) startingMemLocation + i;
+				printf(", %d", *nextShortLocation);
 				break;
 			case CHAR:
-				nextCharLocation = (char *) startingMemLocation
-			 + i;
-				printf("%d \n", *nextCharLocation);
+				nextCharLocation = (char *) startingMemLocation + i;
+				printf(", %d", *nextCharLocation);
 				break;
 		
 			default:
 				break;
 		}
 	}
+	printf("]\n");
 }
