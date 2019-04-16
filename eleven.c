4cc7d4fe3592bb10331acce6d0d0cfc65725bed4
Javi Gomez <rtomyj@gmail.com>
2019-04-15 21:38:42 -0500

    documentation

--- eleven.c
+++ eleven.c
@@ -2,6 +2,10 @@
 #include "libs/algorithm.h"
 #include "libs/print.h"
 
+/*
+	Main functionality.
+	Completes algorithm 11.
+*/
 int main()
 {
 	int x[] = {0, 2, 0, 3, 0, 5, 6, 0, 0};
@@ -10,15 +14,23 @@ int main()
 	unsigned xSize = sizeof(x) / sizeof(int);
 	unsigned ySize = sizeof(y) / sizeof(int);
 
-	int emptyCells[xSize + ySize];
+	int emptyCells[xSize - ySize];	// indices of all 0's (empty cells) found in x
 
-	unsigned xInd = 0, yInd = 0;
-	unsigned currentEmptyCell = 0;
-	unsigned maxEmptyCell = 0;
+	unsigned xInd = 0, yInd = 0;	// which indexes in lists (x, y) algorithm is currently comparing
+	unsigned currentEmptyCell = 0;	// which element to use from emptyCells array
+	unsigned maxEmptyCell = 0;	// how many empty cells have been seen in x and added to emptyCells
 
-
+	/*
+		Compares all elements of x with elements of y.
+		Terminates once all elements of y have been compared and merged into x
+	*/
 	while (yInd < ySize)
 	{
+		/*
+			Condition is met once all elements of x have been traversed.
+			This happens when the y list has elements greater than all elements of x. All remaining elements of y are added 
+			to unused indexes of x.
+		*/
 		if (xInd == xSize)
 		{
 			if (currentEmptyCell < maxEmptyCell)
@@ -30,25 +42,42 @@ int main()
 			continue;
 		}
 
+		/*
+			Current elements.
+		*/
 		int xEle = x[xInd];
 		int yEle = y[yInd];
 
+		/*
+			Unused spot in x is found. 
+			Must be traversed over.
+		*/
 		if (xEle == 0)
 		{
 			emptyCells[maxEmptyCell] = xInd;
 			++xInd;
 			++maxEmptyCell;
 		}
+		/*
+			Comparison will be done to element of x with element of y.
+		*/
 		else
 		{
 			if (xEle > yEle)
 			{
+				/*
+					If there is an emptyCell in x then put the y element into the left most empty cell.
+				*/
 				if (currentEmptyCell < maxEmptyCell)
 				{
 					x[emptyCells[currentEmptyCell]] = yEle;
 					++yInd;
 					++ currentEmptyCell;
 				}
+				/*
+					If there is no emptyCell then swap the x element with the y element.
+					After the swap, the y element is the greater of the two and we are ok to increment the x index.
+				*/
 				else
 				{
 					swapElement(x, y, xInd, yInd);	
@@ -58,6 +87,11 @@ int main()
 			}
 			else
 			{
+				/*
+					The x value is less than the y value meaning we still need to find a spot for the y element (don't increment y).
+					If there is an emptyCell then the x element needs to go to the left most emptyCell.
+					Otherwise, x is already sorted and no swap is needed but the x index needs to be incremented.
+				*/
 				if (currentEmptyCell < maxEmptyCell)
 				{
 					x[emptyCells[currentEmptyCell]] = xEle;
--- nine.c
+++ nine.c
@@ -2,45 +2,70 @@
 #include "libs/algorithm.h"
 #include "libs/print.h"
 
-void swapSort(int items[], unsigned size, int lookingFor, unsigned* unsortedIndex)
-{
-	for (int index = *unsortedIndex; index < size; index++)
-	{
-		if (lookingFor == items[index])
-		{
-			swapElement2(items, *unsortedIndex, index);
-			++(*unsortedIndex);
-		}
-	}
-}
+void swapSort(int[], unsigned, int, unsigned*);
+void sort(int[], unsigned, unsigned);
 
-void sort(int items[], unsigned size, unsigned asc)
-{
-	int lookingFor = 0;
-	int increment = 1;
-
-	if (asc != 0)
-	{
-		lookingFor = 2;
-		increment = -1;
-	}
-
-	unsigned unsortedIndex = 0;
-	swapSort(items, size, lookingFor, &unsortedIndex);
-
-	lookingFor += increment;
-	swapSort(items, size, lookingFor, &unsortedIndex);
-}
-
+/*
+	Main method.
+	Completes algorithm 9.
+*/
 int main()
 {
 	int items[] = {0, 1, 2, 2, 1, 0, 0, 2, 0, 1, 1, 0};
 	const unsigned size = sizeof(items) / sizeof(int);
 
-	sort(items, size, 0);
+	sort(items, size, 0);	// sorts 0's, 1's and 2's in ascending order
 	printArr(items, size, INT);
 
-	sort(items, size, 1);
+	sort(items, size, 1);	// sorts 0's, 1's and 2's in descending order
 	printArr(items, size, INT);
 	return 0;
+}
+
+
+/*
+	Sorts an array that contains 0's, 1's and 2's.
+	Sorting should use constant space and be done in linear time.
+*/
+void sort(int items[], unsigned size, unsigned asc)
+{
+	/*
+		Default values for ascending sort.
+	*/
+	int leftMostValue = 0;
+	int increment = 1;
+
+	/*
+		Changes values for descending sort.
+	*/
+	if (asc != 0)
+	{
+		leftMostValue = 2;
+		increment = -1;
+	}
+
+	unsigned unsortedInd = 0;
+	swapSort(items, size, leftMostValue, &unsortedInd);	// partially sorts array (either 0 (asc) or 2 (desc) are all sorted)
+	swapSort(items, size, (leftMostValue + increment), &unsortedInd);	// array is sorted
+}
+
+
+/*
+	Sorts an array partially by swapping a desired element with an index not yet sorted.
+	Ideally to sort an array this method will be called multiple times.
+	If there are n possible values for an element then this method should be called n-1 times.
+*/
+void swapSort(int items[], unsigned size, int lookingFor, unsigned* unsortedInd)
+{
+	for (int ind = *unsortedInd; ind < size; ind++)
+	{
+		/*
+			Swaps current value with the value at the unsorted index if current value is the current wanted value.
+		*/
+		if (lookingFor == items[ind])
+		{
+			swapElement2(items, *unsortedInd, ind);
+			++(*unsortedInd);	// array is partially sorted, increment index to get to new unsorted index
+		}
+	}
 }
